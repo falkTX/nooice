71c2af78d4b1798b2a1e3edd5e6874ac45279597
falkTX <falktx@gmail.com>
2016-05-24 13:00:03 +0200

    Don't use internal client with udev, can't close automatically

--- noice-udev-register.sh
+++ noice-udev-register.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -e
-
 if [ -z "$DEVNAME" ]; then
+  exit 0
+fi
+if [ ! -e "$DEVNAME" ]; then
   exit 0
 fi
 
 /usr/bin/jack_wait -w
-/usr/bin/jack_load -w noice -i "$DEVNAME"
+exec /usr/bin/noice "$DEVNAME"
+
+# /usr/bin/jack_load noice -i "$DEVNAME"
+# while [ -e "$DEVNAME" ]; do /usr/bin/sleep 1; done
+# /usr/bin/jack_unload noice
