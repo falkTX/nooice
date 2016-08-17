40b4c0f249d3af2e606bae1af34d0bfbb0778590
falkTX <falktx@gmail.com>
2016-08-17 13:54:04 +0200

    Forgot special case about joysticks

--- systemd/nooice-systemd-start.sh
+++ systemd/nooice-systemd-start.sh
@@ -7,6 +7,12 @@ fi
 
 DEVICE_PATH="$1"
 MODULE_NAME="nooice"
-CLIENT_NAME="nooice$(echo ${DEVICE_PATH} | tr -d a-z/)"
+CLIENT_NUMB=$(echo ${DEVICE_PATH} | tr -d a-z/)
+
+if (echo ${DEVICE_PATH} | grep -q "/dev/input/js"); then
+  CLIENT_NUMB=$((${CLIENT_NUMB}+20))
+fi
+
+CLIENT_NAME="nooice${CLIENT_NUMB}"
 
 exec /usr/bin/jack_load -a -w -i ${DEVICE_PATH} ${CLIENT_NAME} ${MODULE_NAME}
